300f2520a55f831d83bdc67bc6e02f4939c20668
yats1304 <chaubaly@gmail.com>
2025-08-08 20:31:38 +0530

    NavBar Added

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+const Navigation = () => {
+  return (
+    <div>
+        <nav>
+        <div className="logo">
+          <img src="/public/images/brand_logo.png" alt="logo"/>
+        </div>
+        <ul>
+          <li href='#'>Menu</li>
+          <li href='#'>Location</li>
+          <li href='#'>About</li>
+          <li href='#'>Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation
